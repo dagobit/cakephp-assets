bf6be917493795c808b77ce56fca7dbfda2b20a7
Uli Renz <uli@dagobit.com>
2020-01-10 16:36:55 +0100

    initial restore of repository

---
+++ config/routes.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * This file is part of cakephp-assets.
+ *
+ * Licensed under The MIT License
+ * For full copyright and license information, please see the LICENSE.txt
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright   Copyright (c) Mirko Pagliai
+ * @link        https://github.com/mirko-pagliai/cakephp-assets
+ * @license     https://opensource.org/licenses/mit-license.php MIT License
+ */
+
+use Cake\Routing\RouteBuilder;
+use Cake\Routing\Router;
+
+Router::plugin('Assets', ['path' => '/assets'], function (RouteBuilder $routes) {
+    $routes->connect('/:filename', ['controller' => 'Assets', 'action' => 'asset'], [
+        'filename' => '[\d\w]+\.(css|js)',
+        'pass' => ['filename'],
+    ]);
+});
